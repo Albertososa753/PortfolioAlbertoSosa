98c5d1c3fcdcbba9f374ecb3797290bdefe066ea
Albertososa753 <betososa753@gmail.com>
2023-09-20 10:31:47 -0300

    .

--- porfolio/src/components/Intro/intro.jsx
+++ porfolio/src/components/Intro/intro.jsx
@@ -9,7 +9,7 @@ const Intro = () => {
 <section id='intro'>
   <div className='introContent'>
 <span className='hello'>Hola,</span>
-<span className="introText">Yo soy <span className="introName">Alberto Sosa</span><br />Desarrollador FullStack</span>
+<span className="introText">Soy <span className="introName">Alberto Sosa</span><br />Desarrollador FullStack</span>
 <p className="introPara">Este es mi sitio web en el cual te mostrae los proyectos que he <br />echo  a lo largo de mi carrera</p>
 <Link><button className="btn"><img src={btnImg} alt="" className='btnImg' />Contratame</button></Link>
   </div>
